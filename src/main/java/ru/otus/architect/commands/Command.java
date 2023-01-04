04620e75813a08e15560a9c2904c66360fea5d25
Белоушкин Александр Николаевич <gudun.ku@gmail.com>
2023-01-04 21:48:33 +0300

    Add command interface

---
+++ src/main/java/ru/otus/architect/commands/Command.java
@@ -0,0 +1,5 @@
+package ru.otus.architect.commands;
+
+public interface Command {
+    void execute();
+}
--- src/main/java/ru/otus/architect/commands/MoveCommand.java
+++ src/main/java/ru/otus/architect/commands/MoveCommand.java
@@ -2,13 +2,14 @@ package ru.otus.architect.commands;
 
 import ru.otus.architect.transforms.Movable;
 
-public class MoveCommand {
+public class MoveCommand implements Command {
     private Movable m;
 
     public MoveCommand(Movable m) {
         this.m = m;
     }
 
+    @Override
     public void execute() {
         m.setPosition(m.getPosition().plus(m.getVelocity()));
     }
