063f11be953ad0aae0410f399be5633a9b01b54b
Mikhail <kilai.ru@yandex.ru>
2023-01-27 13:38:16 +0900

    Механизм обработки исключений в игре "Космическая битва"
    Цель:
    Научится писать различные стратегии обработки исключений так, чтобы соответствующий блок try-catсh не приходилось модифицировать каждый раз, когда возникает потребность в обработке исключительной ситуации по-новому.

--- src/main/java/ru/otus/architect/exceptionHandling/ExceptionHandlerService.java
+++ src/main/java/ru/otus/architect/exceptionHandling/ExceptionHandlerService.java
@@ -5,7 +5,7 @@ import ru.otus.architect.exceptionHandling.handlers.CommandExceptionHandler;
 
 public interface ExceptionHandlerService {
 
-    void handleException(Command command, Exception exception);
+    void getHandleException(Class<? extends Command> commandClazz, Class<? extends Exception> exceptionClazz);
 
     void registerHandlingRule(Class<? extends Command> commandClazz, Class<? extends Exception> exceptionClazz, CommandExceptionHandler exceptionHandler);
 
